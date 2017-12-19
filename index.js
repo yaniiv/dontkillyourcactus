2ae3255ae0b7437c9f547138878bdb3464b43327
na iram <nairam@nas-MacBook-Pro.local>
2017-12-18 22:57:51 -0800

    reformat a html

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
-  console.warn('hi')
-  //
-  // var waypoint = new Waypoint({
-  //   element: document.getElementsByClassName('watering-can-span'),
-  //   handler: function() {
-  //     notify('Basic waypoint triggered')
-  //   }
-  // })
+  console.warn('document ready')
+  $('.watering-can-span').css('opacity', 0);
+
 
   $(".watering-can-span").waypoint(function(direction) {
 
     if (direction === 'down') {
       console.warn('down')
+      $('.watering-can-span').addClass('fadeInLeft');
+      $('.watering-can-span').removeClass('fadeOutLeft');
       // user is scrolling down, reveal our content
     } else if (direction === 'up') {
       console.warn('up')
+      $('.watering-can-span').addClass('fadeOutLeft');
+      $('.watering-can-span').removeClass('fadeInLeft');
     }
-
-  });
-}
-)
+  }, { offset: '50%' })
+})
