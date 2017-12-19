b13f3c38185365ea7fb7c7267eaec359fe766777
na iram <nairam@nas-MacBook-Pro.local>
2017-12-18 22:44:14 -0800

    setup cdn for waypoints.js, animate.css

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
   console.warn('hi')
+  //
+  // var waypoint = new Waypoint({
+  //   element: document.getElementsByClassName('watering-can-span'),
+  //   handler: function() {
+  //     notify('Basic waypoint triggered')
+  //   }
+  // })
 
-  $(".watering-can-span").click(function() {
-    console.warn('poop')
-    $( this ).addClass('animated')
-  })
+  $(".watering-can-span").waypoint(function(direction) {
+
+    if (direction === 'down') {
+      console.warn('down')
+      // user is scrolling down, reveal our content
+    } else if (direction === 'up') {
+      console.warn('up')
+    }
+
+  });
 }
 )
