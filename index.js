2134c2a62ccf679efd4b356ef277f6dbfd64b697
yantonsoup <ygoldobin@gmail.com>
2017-12-21 11:24:26 -0800

    adjust waypoints to raw js syntax

--- index.js
+++ index.js
@@ -2,32 +2,36 @@ $(document).ready(function() {
   console.warn('document ready')
 
   // Watering Can
-  $('.watering-can-span').css('opacity', 0);
-  $(".watering-can").waypoint(function(direction) {
-    if (direction === 'down') {
-      console.warn('down')
-      $('.watering-can').addClass('slideInRight');
-      $('.watering-can').removeClass('slideOutRight');
-      // user is scrolling down, reveal our content
-    } else if (direction === 'up') {
-      console.warn('up')
-      $('.watering-can').addClass('slideOutRight');
-      $('.watering-can').removeClass('slideInRight');
-    }
+  $('.watering-can').css('opacity', 0);
+  var waypoint = new Waypoint({
+    element: document.getElementsByClassName('section-2'),
+    handler: function(direction) {
+      $('.watering-can').css('opacity', 100);
+      console.warn('MOVING ' + direction + '. I am 200px from the top of the section2')
+      if (direction === 'down') {
+        $('.watering-can').addClass('slideInRight');
+        $('.watering-can').removeClass('slideOutRight');
+      } else if (direction === 'up') {
+        $('.watering-can').addClass('slideOutRight');
+        $('.watering-can').removeClass('slideInRight');
+      }
+    },
+    offset: 200
   })
 
-  // Watering Can
   $('.sunflower').css('opacity', 0);
-  $(".sunflower").waypoint(function(direction) {
-    if (direction === 'down') {
-      console.warn('down')
-      $('.sunflower').addClass('rotateInDownLeft');
-      $('.sunflower').removeClass('rotateOutDownRight');
-      // user is scrolling down, reveal our content
-    } else if (direction === 'up') {
-      console.warn('up')
-      $('.sunflower').addClass('rotateOutDownRight');
-      $('.sunflowern').removeClass('rotateInDownLeft');
-    }
+  var waypoint = new Waypoint({
+    element: document.getElementsByClassName('section-3'),
+    handler: function(direction) {
+      console.warn('MOVING ' + direction + '. I am 200px from the top of the section2')
+      if (direction === 'down') {
+        $('.sunflower').addClass('rotateInDownLeft');
+        $('.sunflower').removeClass('rotateOutDownRight');
+      } else if (direction === 'up') {
+        $('.sunflower').addClass('rotateOutDownRight');
+        $('.sunflowern').removeClass('rotateInDownLeft');
+      }
+    },
+    offset: 200
   })
 })
